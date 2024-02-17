6c5b7addc0fe901c40657a7738000cd4ed89086e
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-02-17 16:47:51 +0900

    src/index.tsに初めてのAppを書いてみた

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', (c: any) => {
+  return c.text('Hello, my first Hono app!')
 })
 
 export default app
