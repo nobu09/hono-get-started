43abd59f1025437bd655b11cdbf6a3340dc8133e
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-02-17 17:09:41 +0900

    Request and Response

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,19 @@ app.get('/', (c: any) => {
   return c.text('Hello, my first Hono app!')
 })
 
+app.get('/posts/:id', (c: any) => {
+  const page = c.req.query('page')
+  const id = c.req.param('id')
+  c.header('X-Message', 'Hi!')
+
+  return c.text(`You want see ${page} of ${id}`)
+})
+
+// POST
+app.post('/posts', (c) => c.text('Created!', 201))
+
+// DELETE
+app.delete('/posts/:id', (c) => c.text(`${c.req.param('id')} is deleted!`))
+
 export default app
+
